109d44b201c2862b8a59f6550329ab369c731c7e
Stephen Jackson <scj7t4@mst.edu>
2011-10-17 10:00:41 -0500

    Added more to the fab file

--- Broker/fabric/fabfile.py
+++ Broker/fabric/fabfile.py
@@ -1,8 +1,11 @@
 from fabric.api import *
 
 env.key_filename = ["/home/scj7t4/.ssh/id_rsa"]
+env.warn_only = True
 
 BOOST_ROOT = '~/boost'
+BROKER_DIR = '~/FREEDM/Broker'
+SRC_DIR = '~/FREEDM/Broker/src'
 
 def host_type():
     run('uname -s')
@@ -12,25 +15,37 @@ def change_and_check():
         run('pwd')
 
 def pull_checkout(branch='master'):
-    """
-    This command will checkout a branch and run it. It is meant
-    to be run on nodes which clone from the main one, not the 
-    one that interfaces directly with github.
-    """
-    with cd('~/FREEDM/Broker/src'):
+    with cd(SRC_DIR):
         result = run('git branch | grep %s' % branch)
         if result.return_code != 0:
             result = run('git branch -r | grep %s' % branch)
             if result.return_code != 0:
                 run('git checkout --track -b %s origin/%s' % (branch,branch))
+            else:
+                print "Couldn't get into the specified branch! (Not Found)"
+                exit(1)
         else:
             run('git checkout %s' % branch)
         run('git pull')
  
 def build():
-    with cd('~/FREEDM/Broker'):
+    with cd(BROKER_DIR):
         run("BOOST_ROOT='%s' cmake ." % BOOST_ROOT)
-    with cd('~/FREEDM/Broker/src'):
+    with cd(SRC_DIR):
         run("BOOST_ROOT='%s' make" % BOOST_ROOT)
 
+def start_sim(runtime='10m'):
+    with cd(SRC_DIR):
+        result = run("screen -dR test timeout %s ./PosixBroker" % runtime)
+        if result.return_code != 0:
+            print "Test failed with error code."
+
+def wait_sim(runtime='10m'):
+    print "Waiting for test."
+    local("sleep %s" % runtime)
+    print "Done Waiting"
+
+def setup_sim(expconfig):
+    with cd(SRC_DIR):
+        put(expconfig,'./network.xml')
 

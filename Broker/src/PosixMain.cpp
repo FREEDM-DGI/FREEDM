f560580685a40fddc5e1497d74424a69b759f7d0
Michael Catanzaro <michael.catanzaro@mst.edu>
2012-03-05 15:32:17 -0600

    PosixMain now has <=80 characters per line

--- Broker/src/PosixMain.cpp
+++ Broker/src/PosixMain.cpp
@@ -1,35 +1,26 @@
-////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 /// @file      PosixMain.cpp
 ///
 /// @author    Derek Ditch <derek.ditch@mst.edu>
 ///
-/// @compiler  C++
-///
 /// @project   FREEDM DGI
 ///
-/// @description Main entry point for POSIX systems for the Broker
-/// system and accompanying software modules.
+/// @description Main entry point for POSIX systems for the Broker system and
+///  accompanying software modules.
 ///
-/// @license
-/// These source code files were created at as part of the
-/// FREEDM DGI Subthrust, and are intended for use in teaching or
-/// research.  They may be freely copied, modified and redistributed
-/// as long as modified versions are clearly marked as such and
-/// this notice is not removed.
+/// These source code files were created at the Missouri University of Science
+/// and Technology, and are intended for use in teaching or research. They may
+/// be freely copied, modified and redistributed as long as modified versions
+/// are clearly marked as such and this notice is not removed.
 ///
-/// Neither the authors nor the FREEDM Project nor the
-/// National Science Foundation
-/// make any warranty, express or implied, nor assumes
-/// any legal responsibility for the accuracy,
-/// completeness or usefulness of these codes or any
-/// information distributed with these codes.
+/// Neither the authors nor Missouri S&T make any warranty, express or implied,
+/// nor assume any legal responsibility for the accuracy, completeness or
+/// usefulness of these files or any information distributed with these files.
 ///
-/// Suggested modifications or questions about these codes
-/// can be directed to Dr. Bruce McMillin, Department of
-/// Computer Science, Missouri University of Science and
-/// Technology, Rolla, MO  65409, (ff@mst.edu).
-///
-////////////////////////////////////////////////////////////////////
+/// Suggested modifications or questions about these files can be directed to
+/// Dr. Bruce McMillin, Department of Computer Science, Missouri University of
+/// Science and Technology, Rolla, MO 65401 <ff@mst.edu>.
+////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include <string>
@@ -70,14 +61,16 @@ using namespace freedm;
 #include <signal.h>
 CREATE_STD_LOGS()
 
+/// Returns the filename without directory path.
 std::string basename(const std::string &s)
-// Returns the filename without directory path
-// This works for both Windows and UNIX-style paths
 {
+    // This works for both Windows and UNIX-style paths
     size_t idx;
     idx = s.find_last_of("/\\");
     return s.substr(idx + 1);
 }
+
+/// Broker entry point
 int main(int argc, char* argv[])
 {
     Logger::Log::setLevel(3);
@@ -106,35 +99,40 @@ int main(int argc, char* argv[])
     {
         // Check command line arguments.
         genOpts_.add_options()
-                ("help,h", "print usage help (this screen)")
-                ("version,V", "print version info")
-                ("config,c", po::value<std::string > (&cfgFile_)->
-                default_value("freedm.cfg"), "filename of additional configuration.")
-                ("generateuuid,g", po::value<std::string > (&uuidgenerator)->
-                default_value(""), "Generate a uuid for the specified host, output it, and exit")
-                ("uuid,u", "Print this node's generated uuid and exit");
+                ( "help,h", "print usage help (this screen)" )
+                ( "version,V", "print version info" )
+                ( "config,c", po::value<std::string > ( &cfgFile_ )->
+                default_value("freedm.cfg"),
+                "filename of additional configuration." )
+                ( "generateuuid,g", po::value<std::string > ( &uuidgenerator )->
+                default_value(""), "Generate a uuid for the specified host, " +
+                " output it, and exit" )
+                ( "uuid,u", "Print this node's generated uuid and exit" );
         // This is for arguments in a config file or as arguments
         configOpts_.add_options()
-                ("add-host", po::value<std::vector<std::string> >()->
-                composing(), "peer hostname:port pair")
-                ("address", po::value<std::string > (&listenIP_)->
-                default_value("0.0.0.0"), "IP interface to listen on")
-                ("port,p", po::value<std::string > (&port_)->
-                default_value("1870"), "TCP port to listen on")
-                ("add-device,d", po::value<std::vector<std::string> >()->
-                composing(), "physical device name:type pair")
-                ("client-host,l", po::value<std::string > (&interHost)->
-                default_value(""), "Hostname to use for the lineclient/RTDSclient to connect.")
-                ("client-port,q", po::value<std::string > (&interPort)->
-                default_value("4001"), "The port to use for the lineclient/RTDSclient to connect.")
-                ("xml,x", po::value<std::string > (&xml)->default_value("FPGA.xml"),
-                "filename of FPGA message specification")
-                ("verbose,v", po::value<int>(&verbose_)->
+                ( "add-host", po::value<std::vector<std::string> >( )->
+                composing(), "peer hostname:port pair" )
+                ( "address", po::value<std::string > ( &listenIP_ )->
+                default_value("0.0.0.0"), "IP interface to listen on" )
+                ( "port,p", po::value<std::string > ( &port_ )->
+                default_value("1870"), "TCP port to listen on" )
+                ( "add-device,d", po::value<std::vector<std::string> >( )->
+                composing(), "physical device name:type pair" )
+                ( "client-host,l", po::value<std::string > ( &interHost )->
+                default_value(""), "Hostname to use for the " +
+                "lineclient/RTDSclient to connect." )
+                ( "client-port,q", po::value<std::string > ( &interPort )->
+                default_value("4001"), "The port to use for the "
+                + "lineclient/RTDSclient to connect." )
+                ( "xml,x", po::value<std::string >
+                ( &xml )->default_value("FPGA.xml"),
+                "filename of FPGA message specification" )
+                ( "verbose,v", po::value<int>( &verbose_ )->
                 implicit_value(5)->default_value(3),
-                "enable verbose output (optionally specify level)");
+                "enable verbose output (optionally specify level)" );
         hiddenOpts_.add_options()
-                ("setuuid", po::value<std::string > (&uuid_),
-                "UUID for this host");
+                ( "setuuid", po::value<std::string > ( &uuid_ ),
+                "UUID for this host" );
 
         // Specify positional arguments
         posOpts_.add("address", 1).add("port", 1);
@@ -276,15 +274,15 @@ int main(int argc, char* argv[])
         {
             using namespace broker::device;
             std::vector< std::string > device_list =
-                    vm_["add-device"].as< std::vector<std::string> >();
+                    vm_["add-device"].as< std::vector<std::string> >( );
             foreach(std::string &devid, device_list)
             {
                 int idx_ = devid.find(':');
 
-                if (idx_ != static_cast<int> (std::string::npos))
+                if (idx_ != static_cast<int> ( std::string::npos ))
                 {
                     std::string DevName_(devid.begin(), devid.begin() + idx_),
-                            DevType_(devid.begin() + (idx_ + 1), devid.end());
+                            DevType_(devid.begin() + ( idx_ + 1 ), devid.end());
 
                     if (m_phyManager->DeviceExists(DevName_))
                     {
@@ -300,7 +298,7 @@ int main(int argc, char* argv[])
                     else if (DevType_ == "DESD")
                     {
                         CDeviceFactory::CreateDevice("DESD", DevName_);
-                        Logger::Info << "Added DESD device: " << DevName_ 
+                        Logger::Info << "Added DESD device: " << DevName_
                                 << std::endl;
                     }
                     else if (DevType_ == "LOAD")
@@ -319,7 +317,7 @@ int main(int argc, char* argv[])
                 {
                     if (m_phyManager->DeviceExists(devid))
                     {
-                        Logger::Warn << "Duplicate device: " << devid 
+                        Logger::Warn << "Duplicate device: " << devid
                                 << std::endl;
                     }
                     else
@@ -352,32 +350,35 @@ int main(int argc, char* argv[])
         GMAgent GM_(uuidstr, broker_.GetIOService(), dispatch_, m_conManager);
         dispatch_.RegisterReadHandler("gm", &GM_);
         // Instantiate and register the power management module
-        lbAgent LB_(uuidstr, broker_.GetIOService(), dispatch_, m_conManager, *m_phyManager);
+        lbAgent LB_(uuidstr, broker_.GetIOService(), dispatch_, m_conManager,
+                *m_phyManager);
         dispatch_.RegisterReadHandler("lb", &LB_);
         // Instantiate and register the state collection module
-        SCAgent SC_(uuidstr, broker_.GetIOService(), dispatch_, m_conManager, *m_phyManager);
+        SCAgent SC_(uuidstr, broker_.GetIOService(), dispatch_, m_conManager,
+                *m_phyManager);
         dispatch_.RegisterReadHandler("any", &SC_);
 
-        // The peerlist should be passed into constructors as references or pointers
-        // to each submodule to allow sharing peers. NOTE this requires thread-safe
-        // access, as well. Shouldn't be too hard since it will mostly be read-only
+        // The peerlist should be passed into constructors as references or 
+        // pointers to each submodule to allow sharing peers. NOTE this requires
+        // thread-safe access, as well. Shouldn't be too hard since it will 
+        // mostly be read-only
         if (vm_.count("add-host"))
         {
             std::vector< std::string > arglist_ =
-                    vm_["add-host"].as< std::vector<std::string> >();
+                    vm_["add-host"].as< std::vector<std::string> >( );
             foreach(std::string &s_, arglist_)
             {
                 int idx_ = s_.find(':');
 
-                if (idx_ == static_cast<int> (std::string::npos))
+                if (idx_ == static_cast<int> ( std::string::npos ))
                 { // Not found!
-                    std::cerr << "Incorrectly formatted host in config file: " <<
-                            s_ << std::endl;
+                    std::cerr << "Incorrectly formatted host in config file: "
+                            << s_ << std::endl;
                     continue;
                 }
 
                 std::string host_(s_.begin(), s_.begin() + idx_),
-                        port1_(s_.begin() + (idx_ + 1), s_.end());
+                        port1_(s_.begin() + ( idx_ + 1 ), s_.end());
                 // Construct the UUID of host from its DNS
                 uuid u1_ = uuid::from_dns(host_);
                 //Load the UUID into string

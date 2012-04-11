6180598e2521ad25a8b1b9ae26f71a00f17e4c98
Stephen Jackson <scj7t4@gmail.com>
2012-04-11 09:34:13 -0500

    Adding more header data to sent messages.

--- Broker/src/CConnection.cpp
+++ Broker/src/CConnection.cpp
@@ -132,6 +132,9 @@ void CConnection::Send(CMessage p_mesg)
     // Queue.
     if(GetUUID() == GetConnectionManager().GetUUID())
     {
+        p_mesg.SetSourceUUID(GetConnection()->GetConnectionManager().GetUUID());
+        p_mesg.SetSourceHostname(GetConnection()->GetConnectionManager().GetHostname());
+        p_mesg.SetSendTimestampNow();
         GetDispatcher().HandleRequest(GetBroker(),p_mesg);
         return;
     }

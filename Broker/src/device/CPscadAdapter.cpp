dd1c0bd98e264aa4494ba34393dd44a28ce34c39
tprfh7 <tprfh7@mst.edu>
2012-08-29 00:07:24 -0500

    Massive cleanup of CPscadAdapter.  It likely doesn't compile.

--- Broker/src/device/CPscadAdapter.cpp
+++ Broker/src/device/CPscadAdapter.cpp
@@ -6,7 +6,7 @@
 ///
 /// @project       FREEDM DGI
 ///
-/// @description   Client side implementation of the simulation line protocol.
+/// @description   Client side implementation of the PSCAD line protocol.
 ///
 /// These source code files were created at Missouri University of Science and
 /// Technology, and are intended for use in teaching or research. They may be
@@ -21,9 +21,14 @@
 /// Science and Technology, Rolla, MO 65409 <ff@mst.edu>.
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "CPscadAdapter.hpp"
 #include "CLogger.hpp"
-#include "device/CPscadAdapter.hpp"
 
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+
+#include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
 
 namespace freedm {
@@ -31,25 +36,61 @@ namespace broker {
 namespace device {
 
 namespace {
-
 /// This file's logger.
 CLocalLogger Logger(__FILE__);
-
 }
 
-CPscadAdapter::Pointer CPscadAdapter::Create(boost::asio::io_service & service)
+////////////////////////////////////////////////////////////////////////////////
+/// Constructs a shared instance of a PSCAD adapter.
+///
+/// @pre None.
+/// @post Allocates memory for a new CPscadAdapter object.
+/// @param service The i/o service for the adapter.
+/// @param details The property tree specification of the new adapter.
+/// @return A shared pointer to the newly created adapter.
+///
+/// @limitations None.
+////////////////////////////////////////////////////////////////////////////////
+CPscadAdapter::Pointer CPscadAdapter::Create(boost::asio::io_service & service,
+        const boost::property_tree::ptree & details)
 {
     Logger.Trace << __PRETTY_FUNCTION__ << std::endl;
-    return CPscadAdapter::Pointer(new CPscadAdapter(service));
+    return CPscadAdapter::Pointer(new CPscadAdapter(service, details));
 }
 
-CPscadAdapter::CPscadAdapter(boost::asio::io_service & service)
-: IConnectionAdapter(service)
+////////////////////////////////////////////////////////////////////////////////
+/// Connects the adapter to its remote host to start communication.
+///
+/// @pre m_host and m_port must specify a valid remote host.
+/// @post m_socket can be used to communicate with the remote host.
+///
+/// @limitations None.
+////////////////////////////////////////////////////////////////////////////////
+void CPscadAdapter::Start()
 {
     Logger.Trace << __PRETTY_FUNCTION__ << std::endl;
+    Connect(m_host, m_port);
+    
+    Logger.Notice << "The PSCAD adapter has started its connection to "
+            << m_host << ":" << m_port << "." << std::endl;
 }
 
-void CPscadAdapter::Set(const Identifier device, const SettingKey key,
+////////////////////////////////////////////////////////////////////////////////
+/// Sends a request to the remote host to update the value of its device signal
+/// to a specified value.  This call will block until an acknowledgement of the
+/// request is received.
+///
+/// @ErrorHandling Throws a std::runtime_error if the connection is invalid or
+/// the remote host failed to handle the request.
+/// @pre m_socket must be initialized through CPscadAdapter::Start.
+/// @post Communicates the request to the remote host through m_socket.
+/// @param device The device whose value should be updated.
+/// @param signal The signal of the device to update.
+/// @param value The new value of the device's signal.
+///
+/// @limitations None.
+////////////////////////////////////////////////////////////////////////////////
+void CPscadAdapter::Set(std::string device, std::string signal,
         const SettingValue value)
 {
     Logger.Trace << __PRETTY_FUNCTION__ << std::endl;
@@ -60,29 +101,54 @@ void CPscadAdapter::Set(const Identifier device, const SettingKey key,
     boost::asio::streambuf response;
     std::istream response_stream(&response);
     std::string response_code, response_message;
-
+    
+    // check for a valid connection
+    if( !m_socket.is_open() )
+    {
+        std::stringstream ss;
+        ss << "Failed to handle request: socket not open." << std::endl;
+        throw std::runtime_error(ss.str());
+    }
+    
     // format and send the request stream
-    request_stream << "SET " << device << ' ' << key << ' ' << value
-            << "\r\n";
+    request_stream << "SET " << device <<' '<< signal <<' '<< value << "\r\n";
+    Logger.Notice << "Sending data through a blocking write." << std::endl;
+    Logger.Debug << "Data has the value: " << request.data() << std::endl;
     boost::asio::write(m_socket, request);
 
     // receive and split the response stream
+    Logger.Notice << "Receiving data through a blocking read." << std::endl;
     boost::asio::read_until(m_socket, response, "\r\n");
+    Logger.Debug << "Received the value: " << response.data() << std::endl;
     response_stream >> response_code >> response_message;
 
     // handle bad responses
-    if (response_code != "200")
+    if( response_code != "200" )
     {
         std::stringstream ss;
-        ss << "CLineClient attempted to set " << key << " to " << value
-                << " on device " << device << ", but received a PSCAD error: "
-                << response_message;
+        ss << "Failed to set (" << device << "," << signal << ") to "
+                << value << " because: " << response_message << std::endl;
         throw std::runtime_error(ss.str());
     }
+    Logger.Info << "Set the value of (" << device << "," << signal << ") to "
+            << value << "." << std::endl;
 }
 
-SettingValue CPscadAdapter::Get(const Identifier device,
-        const SettingKey key) const
+////////////////////////////////////////////////////////////////////////////////
+/// Sends a request to the remote host to retrieve a device signal's value.
+/// The call will block until an acknowledgement of the request is received.
+///
+/// @ErrorHandling Throws a std::runtime_error if the connection is invalid or
+/// the remote host failed to handle the request.
+/// @pre m_socket must be initialized through CPscadAdapter::Start.
+/// @post Communicates the request to the remote host through m_socket.
+/// @param device The device whose value should be retrieved.
+/// @param signal The signal of the device to retrieve.
+/// @return The value of the retrieved device signal.
+///
+/// @limitations None.
+////////////////////////////////////////////////////////////////////////////////
+SettingValue CPscadAdapter::Get(std::string device, std::string signal) const
 {
     Logger.Trace << __PRETTY_FUNCTION__ << std::endl;
 
@@ -92,28 +158,52 @@ SettingValue CPscadAdapter::Get(const Identifier device,
     boost::asio::streambuf response;
     std::istream response_stream(&response);
     std::string response_code, response_message, value;
-
+    
+    // check for a valid connection
+    if( !m_socket.is_open() )
+    {
+        std::stringstream ss;
+        ss << "Failed to handle request: socket not open." << std::endl;        
+        throw std::runtime_error(ss.str());
+    }
+    
     // format and send the request stream
     request_stream << "GET " << device << ' ' << key << "\r\n";
+    Logger.Notice << "Sending data through a blocking write." << std::endl;
+    Logger.Debug << "Data has the value: " << request.data() << std::endl;
     boost::asio::write(m_socket, request);
 
     // receive and split the response stream
+    Logger.Notice << "Receiving data through a blocking read." << std::endl;
     boost::asio::read_until(m_socket, response, "\r\n");
+    Logger.Debug << "Received the value: " << response.data() << std::endl;
     response_stream >> response_code >> response_message >> value;
 
     // handle bad responses
-    if (response_code != "200")
+    if( response_code != "200" )
     {
         std::stringstream ss;
-        ss << "CLineClient attempted to get " << key << " on device "
-                << device << ", but received a PSCAD error: "
-                << response_message;
+        ss << "Failed to get (" << device << "," << signal << ") because: "
+                << response_message << std::endl;
         throw std::runtime_error(ss.str());
     }
-
-    return boost::lexical_cast<SettingValue > ( value );
+    
+    Logger.Info << "Received the value of (" << device << "," << signal
+            << ") as " << value << "." << std::endl;
+    return boost::lexical_cast<SettingValue>(value);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// Sends a request to terminate the connection with a remote host.  The call
+/// will block until an acknowledgement of the request is received.
+///
+/// @ErrorHandling Throws a std::runtime_error if the connection is invalid or
+/// the remote host failed to handle the request.
+/// @pre m_socket must be initialized through CPscadAdapter::Start.
+/// @post Communicates the request to the remote host and closes m_socket.
+///
+/// @limitations None.
+////////////////////////////////////////////////////////////////////////////////
 void CPscadAdapter::Quit()
 {
     Logger.Trace << __PRETTY_FUNCTION__ << std::endl;
@@ -124,39 +214,89 @@ void CPscadAdapter::Quit()
     boost::asio::streambuf response;
     std::istream response_stream(&response);
     std::string response_code, response_message;
-
+    
+    // check for a valid connection
+    if( !m_socket.is_open() )
+    {
+        std::stringstream ss;
+        ss << "Failed to handle request: socket not open." << std::endl;
+        throw std::runtime_error(ss.str());
+    }
+    
     // format and send the request stream
     request_stream << "QUIT\r\n";
+    Logger.Notice << "Sending data through a blocking write." << std::endl;
+    Logger.Debug << "Data has the value: " << request.data() << std::endl;
     boost::asio::write(m_socket, request);
 
     // receive and split the response stream
+    Logger.Notice << "Receiving data through a blocking read." << std::endl;
     boost::asio::read_until(m_socket, response, "\r\n");
+    Logger.Debug << "Received the value: " << response.data() << std::endl;
     response_stream >> response_code >> response_message;
 
     // handle bad responses
     if (response_code != "200")
     {
         std::stringstream ss;
-        ss << "CLineClient attempted quit, but received a PSCAD error: "
-                << response_message;
+        ss << "Failed to end a connection: " << response_message << std::endl;
         throw std::runtime_error(ss.str());
     }
-
+    
     // close connection
     m_socket.close();
+    Logger.Status << "Closed an open socket connection to " << m_host << ":"
+            << m_port << "." << std::endl;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// Closes the socket connection prior to destructing the object.
+///
+/// @pre None.
+/// @post Calls CPscadAdapter::Quit if m_socket is open.
+///
+/// @limitations None.
+////////////////////////////////////////////////////////////////////////////////
 CPscadAdapter::~CPscadAdapter()
 {
     Logger.Trace << __PRETTY_FUNCTION__ << std::endl;
 
     //  perform teardown
-    if (m_socket.is_open())
+    if( m_socket.is_open() )
     {
         Quit();
     }
 }
 
-}//namespace broker
-}//namespace freedm
-}//namespace device
+///////////////////////'////////////////////////////////////////////////////////
+/// Constructs a pscad adapter based on the property tree specification.
+///
+/// @ErrorHandling Throws a std::runtime_error if the property tree is bad.
+/// @SharedMemory Passes the i/o service to IConnectionAdapter.
+/// @pre The property tree must specify the target hostname and port number.
+/// @post Constructs a new adapter instance.
+///
+/// @limitations None.
+////////////////////////////////////////////////////////////////////////////////
+CPscadAdapter::CPscadAdapter(boost::asio::io_service & service,
+        const boost::property_tree::ptree & details)
+    : IConnectionAdapter(service)
+{
+    Logger.Trace << __PRETTY_FUNCTION__ << std::endl;
+    
+    try
+    {
+        m_host = details.get<std::string>("host");
+        m_port = details.get<std::string>("port");
+    }
+    catch( std::exception & e )
+    {
+        std::stringstream ss;
+        ss << "Failed to create adapter: " << e.what() << std::endl;
+        throw std::runtime_error(ss.str());
+    }
+}
+
+} // namespace broker
+} // namespace freedm
+} // namespace device

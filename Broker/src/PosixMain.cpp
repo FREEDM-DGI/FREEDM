926d3f5b0684874ddc43ec2280c59471bb04f44b
Michael Catanzaro <michael.catanzaro@mst.edu>
2012-12-22 11:20:41 -0600

    Pretty up cli/config options
    * --address and --port must now be specified if used on the cli
      (i.e. they are no longer positional arguments)
    
    * -g is now useful if a peer uses a different listening port.
      You can use either -g hostname to assume his listening port is
      the same as yours, as before, or now use -g hostname:port
    
    * Fix --version to actually work
    
    * --list-loggers is no longer valid in freedm.cfg, only on cli
    
    * Help is now sent to stdout instead of stderr
    
    * Remove setuuid option, which was broken and would be hard to fix
    
    * Return 1 instead of -1 if the config file didn't load
    
    Conflicts:
    	Broker/config/samples/freedm.cfg
    	Broker/src/PosixMain.cpp

--- Broker/src/PosixMain.cpp
+++ Broker/src/PosixMain.cpp
@@ -49,10 +49,11 @@
 #include <boost/assign/list_of.hpp>
 #include <boost/bind.hpp>
 #include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/program_options.hpp>
-#include <boost/thread.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
+#include <boost/thread.hpp>
 
 namespace po = boost::program_options;
 
@@ -73,54 +74,51 @@ const unsigned int COPYRIGHT_YEAR = 2013;
 int main(int argc, char* argv[])
 {
     CGlobalLogger::instance().SetGlobalLevel(3);
-    // Variable Declaration
-    po::options_description genOpts("General Options"),
-            configOpts("Configuration"), hiddenOpts("hidden");
-    po::options_description visibleOpts, cliOpts, cfgOpts;
-    po::positional_options_description posOpts;
+    po::options_description genOpts("Command Line Options");
+    po::options_description cfgOpts("General Configuration Options");
+    po::options_description cliOpts; // genOpts + cfgOpts
     po::variables_map vm;
     std::ifstream ifs;
     std::string cfgFile, loggerCfgFile, adapterCfgFile;
-    std::string listenIP, port, uuidString, hostname, uuidgenerator;
+    std::string listenIP, port, hostname, uuidgenerator;
     unsigned int globalVerbosity;
     unsigned short factoryPort;
     CUuid uuid;
 
-    // Load Config Files
     try
     {
-        // Check command line arguments.
+        // These options are only allowed on the command line.
         genOpts.add_options()
-                ( "help,h", "print usage help (this screen)" )
-                ( "version,V", "print version info" )
                 ( "config,c",
                 po::value<std::string > ( &cfgFile )->
                 default_value("./config/freedm.cfg"),
                 "filename of additional configuration." )
                 ( "generateuuid,g",
                 po::value<std::string > ( &uuidgenerator )->default_value(""),
-                "Generate a uuid for the specified host, output it, and exit" )
-                ( "uuid,u", "Print this node's generated uuid and exit" );
+                "generate a uuid for a host or host:port" )
+                ( "help,h", "print usage help (this screen)" )
+                ( "list-loggers,l", "print all available loggers" )
+                ( "uuid,u", "print this node's generated uuid" )
+                ( "version,V", "print version info" );
 
-        // This is for arguments in a config file or as arguments
-        configOpts.add_options()
+        // These options can be specified on command line or in the config file.
+        cfgOpts.add_options()
                 ( "add-host",
                 po::value<std::vector<std::string> >( )->composing(),
-                "peer hostname:port pair" )
+                "hostname:port of a peer" )
                 ( "address",
                 po::value<std::string > ( &listenIP )->default_value("0.0.0.0"),
-                "IP interface to listen on" )
+                "IP interface to listen for peers on" )
                 ( "port,p",
                 po::value<std::string > ( &port )->default_value("1870"),
-                "TCP port to listen on" )
+                "TCP port to listen for peers on" )
                 ( "factory-port", po::value<unsigned short>(&factoryPort)->
                 default_value(1610), "port number for the adapter factory" )
                 ( "adapter-port",
                 po::value<std::vector<std::string> >()->composing(),
                 "available port ranges start:end for ARM adapters" )
                 ( "adapter-config", po::value<std::string>( &adapterCfgFile ),
-                "filename of the adapter specification for physical devices" )
-                ( "list-loggers", "Print all the available loggers and exit" )
+                "filename of the adapter specification" )
                 ( "logger-config",
                 po::value<std::string > ( &loggerCfgFile )->
                 default_value("./config/logger.cfg"),
@@ -129,34 +127,24 @@ int main(int argc, char* argv[])
                 po::value<unsigned int>( &globalVerbosity )->
                 implicit_value(5)->default_value(5),
                 "enable verbose output (optionally specify level)" );
-        hiddenOpts.add_options()
-                ( "setuuid", po::value<std::string > ( &uuidString ),
-                "UUID for this host" );
 
-        // Specify positional arguments
-        posOpts.add("address", 1).add("port", 1);
-        // Visible options
-        visibleOpts.add(genOpts).add(configOpts);
         // Options allowed on command line
-        cliOpts.add(visibleOpts).add(hiddenOpts);
-        // Options allowed in config file
-        cfgOpts.add(configOpts).add(hiddenOpts);
+        cliOpts.add(genOpts).add(cfgOpts);
         // If submodules need custom commandline options
         // there should be a 'registration' of those options here.
         // Other modules should use options of the form: 'modulename.option'
         // This prevents namespace conflicts
         // Add them all to the mapping component
         po::store(po::command_line_parser(argc, argv)
-                .options(cliOpts).positional(posOpts).run(), vm);
+                .options(cliOpts).run(), vm);
         po::notify(vm);
 
         // Read options from the main config file.
         ifs.open(cfgFile.c_str());
         if (!ifs)
         {
-            Logger.Error << "Unable to load config file: "
-                    << cfgFile << std::endl;
-            return -1;
+            std::cerr << "Unable to load config file: " << cfgFile << std::endl;
+            return 1;
         }
         else
         {
@@ -174,17 +162,7 @@ int main(int argc, char* argv[])
 
         if (vm.count("help"))
         {
-            std::cerr << visibleOpts << std::endl;
-            return 0;
-        }
-        if (uuidgenerator != "" || vm.count("uuid"))
-        {
-            if (uuidgenerator == "")
-            {
-                uuidgenerator = boost::asio::ip::host_name();
-            }
-            uuid = CUuid::from_dns(uuidgenerator, port);
-            std::cout << uuid << std::endl;
+            std::cout << cliOpts << std::endl;
             return 0;
         }
 
@@ -197,19 +175,6 @@ int main(int argc, char* argv[])
             return 0;
         }
 
-        if (vm.count("uuid"))
-        {
-            uuid = static_cast<CUuid> ( uuidString );
-            Logger.Info << "Loaded UUID: " << uuid << std::endl;
-        }
-        else
-        {
-            // Try to resolve the host's dns name
-            hostname = boost::asio::ip::host_name();
-            Logger.Info << "Hostname: " << hostname << std::endl;
-            uuid = CUuid::from_dns(hostname,port);
-            Logger.Info << "Generated UUID: " << uuid << std::endl;
-        }
         // Refine the logger verbosity settings.
         CGlobalLogger::instance().SetGlobalLevel(globalVerbosity);
         CGlobalLogger::instance().SetInitialLoggerLevels(loggerCfgFile);
@@ -219,14 +184,61 @@ int main(int argc, char* argv[])
             return 0;
         }
 
-        std::stringstream ss2;
-        std::string uuidstr2;
-        ss2 << uuid;
-        ss2 >> uuidstr2;
+        if (uuidgenerator != "" || vm.count("uuid"))
+        {
+            std::size_t colonPosition = uuidgenerator.find(':');
+            if (colonPosition != std::string::npos) // -g with hostname:port
+            {
+                // need to use his port to generate the uuid, not ours
+                port = uuidgenerator.substr(colonPosition+1);
+                uuidgenerator = uuidgenerator.substr(0, colonPosition);
 
+                // this is just a sanity-check
+                bool badLexicalCast = false;
+                try
+                {
+                    (void) boost::lexical_cast<int>(port);
+                }
+                catch (boost::bad_lexical_cast)
+                {
+                    badLexicalCast = true;
+                }
+                if (port.length() < 4 || port.length() > 5 || badLexicalCast)
+                {
+                  std::cerr << "Tried to generate a UUID using hostname="
+                            << uuidgenerator << ", port=" << port
+                            << ", but this is silly." << std::endl;
+                  return 1;
+                }
+            }
+            else if (uuidgenerator == "") // -u
+            {
+                uuidgenerator = boost::asio::ip::host_name();
+            }
+            // else -g with hostname, but use our port
+
+            uuid = CUuid::from_dns(uuidgenerator, port);
+            std::cout << uuid << std::endl;
+            return 0;
+        }
+
+        // Try to resolve the host's dns name
+        hostname = boost::asio::ip::host_name();
+        Logger.Info << "Hostname: " << hostname << std::endl;
+        uuid = CUuid::from_dns(hostname,port);
+        Logger.Info << "Generated UUID: " << uuid << std::endl;
+
+        // Get the UUID as a string
+        std::string uuidstr;
+        {
+            std::stringstream ss;
+            ss << uuid;
+            ss >> uuidstr;
+        }
+        
         /// Prepare the global Configuration
         CGlobalConfiguration::instance().SetHostname(hostname);
-        CGlobalConfiguration::instance().SetUUID(uuidstr2);
+        CGlobalConfiguration::instance().SetUUID(uuidstr);
         CGlobalConfiguration::instance().SetListenPort(port);
         CGlobalConfiguration::instance().SetListenAddress(listenIP);
         CGlobalConfiguration::instance().SetClockSkew(
@@ -322,11 +334,6 @@ int main(int argc, char* argv[])
         //dispatch_.RegisterWriteHandler( "any", &uuidHandler_ );
         // Run server in background thread
         CBroker broker(listenIP, port, dispatch, ios, conManager);
-        // Load the UUID into string
-        std::stringstream ss;
-        std::string uuidstr;
-        ss << uuid;
-        ss >> uuidstr;
         // Instantiate and register the group management module
         gm::GMAgent GM(uuidstr, broker);
         broker.RegisterModule("gm",boost::posix_time::milliseconds(400));

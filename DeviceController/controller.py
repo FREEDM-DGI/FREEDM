eb234c88e1cc2cacbf55e100be9c00aec18ce5a1
Thomas Roth <tprfh7@mst.edu>
2013-01-09 19:32:20 -0600

    fixed controller data packet format (round 2)

--- DeviceController/controller.py
+++ DeviceController/controller.py
@@ -129,7 +129,7 @@ def sendStates(adapterSock, deviceSignals):
     msg = 'DeviceStates\r\n'
     for (name, signal) in deviceSignals.keys():
         msg += name + ' ' + signal + ' ' + str(deviceSignals[(name, signal)])
-    msg += '\r\n'
+    msg += '\r\n\r\n'
     print 'Sending states to DGI:\n' + msg
     sendAll(adapterSock, msg)
     print 'Sent states to DGI'

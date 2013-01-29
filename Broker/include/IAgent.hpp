6be0ad91f013c99910c494f4a0c0cc9f953f407a
Michael Catanzaro <mike.catanzaro@gmail.com>
2013-01-29 10:03:25 -0600

    Fix indentation in IAgent.hpp

--- Broker/include/IAgent.hpp
+++ Broker/include/IAgent.hpp
@@ -31,25 +31,25 @@ template<class T>
 /// Common functions useful for all sorts of broker modules
 class IAgent
 {
-    public:
-        /// Provides a PeerSet type for a module templated on T
-        typedef std::map<std::string, T> PeerSet;
-        /// Provides a PeerNodePtr templated on T
-        typedef T PeerNodePtr;
-        /// Provides and Iterator templated on T
-        typedef typename std::map<std::string, T>::iterator PeerSetIterator;
-        /// Provides count() for a PeerSet
-        static int CountInPeerSet(PeerSet& ps, T m)
-            { return ps.count(m->GetUUID()); };
-        /// Provides find() for a PeerSet
-        static PeerSetIterator FindInPeerSet(PeerSet& ps, T m)
-            { return ps.find(m->GetUUID()); };
-        /// Provides erase() for a PeerSet
-        static void EraseInPeerSet(PeerSet& ps, T m)
-            { ps.erase(m->GetUUID()); };
-        /// Provides insert() for a PeerSet
-        static void InsertInPeerSet(PeerSet& ps, T m)
-            { ps.insert(std::pair<std::string,T>(m->GetUUID(),m)); };
+public:
+    /// Provides a PeerSet type for a module templated on T
+    typedef std::map<std::string, T> PeerSet;
+    /// Provides a PeerNodePtr templated on T
+    typedef T PeerNodePtr;
+    /// Provides and Iterator templated on T
+    typedef typename std::map<std::string, T>::iterator PeerSetIterator;
+    /// Provides count() for a PeerSet
+    static int CountInPeerSet(PeerSet& ps, T m)
+        { return ps.count(m->GetUUID()); };
+    /// Provides find() for a PeerSet
+    static PeerSetIterator FindInPeerSet(PeerSet& ps, T m)
+        { return ps.find(m->GetUUID()); };
+    /// Provides erase() for a PeerSet
+    static void EraseInPeerSet(PeerSet& ps, T m)
+        { ps.erase(m->GetUUID()); };
+    /// Provides insert() for a PeerSet
+    static void InsertInPeerSet(PeerSet& ps, T m)
+        { ps.insert(std::pair<std::string,T>(m->GetUUID(),m)); };
 };
 
 } // namespace freedm

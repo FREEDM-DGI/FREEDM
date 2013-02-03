bf9d18bf47965b5b89c946496ffa86fd78dbf12b
Michael Catanzaro <michael.catanzaro@mst.edu>
2013-02-03 01:08:06 -0600

    Fix possible slicing error

--- Broker/src/PosixMain.cpp
+++ Broker/src/PosixMain.cpp
@@ -201,7 +201,7 @@ int main(int argc, char* argv[])
                 {
                     (void) boost::lexical_cast<int>(port);
                 }
-                catch (boost::bad_lexical_cast)
+                catch (boost::bad_lexical_cast &)
                 {
                     badLexicalCast = true;
                 }

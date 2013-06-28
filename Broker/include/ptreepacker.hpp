62a14b62e11ded9a0c9e51da29c8c151d2ee3a42
Stephen Jackson <scj7t4@mst.edu>
2013-06-28 15:30:56 -0500

    Make less copies of the input string

--- Broker/include/ptreepacker.hpp
+++ Broker/include/ptreepacker.hpp
@@ -37,47 +37,48 @@ std::string encode_tree(const boost::property_tree::ptree &tree)
             // Encode the subtree
             subr = encode_tree(it->second);
             subt<<'s'<<std::setw(LENGTHFIELD)<<std::setfill('0')<<subr.length();
-            r += out.str() + subt.str() + subr;
+            r += out.str()+subt.str()+subr;
         }
         return r;
     }
 }
 
 
-boost::property_tree::ptree decode_tree(std::string encoded)
+boost::property_tree::ptree decode_tree(const std::string &encoded)
 {
     boost::property_tree::ptree decoded;
-    while(encoded.length() > 0)
+    size_t s = 0;
+    while(s < encoded.length())
     {
-        std::stringstream conv( encoded.substr(STORETYPELEN,LENGTHFIELD) );
+        std::stringstream conv( encoded.substr(s+STORETYPELEN,LENGTHFIELD) );
         size_t fieldlen;
         conv>>fieldlen;
-        char token = encoded[0];
-        encoded = encoded.substr(STORETYPELEN+LENGTHFIELD);
+        char token = encoded[s];
+        s += STORETYPELEN+LENGTHFIELD;
         // Check to see if you're unpacking a value or a key.
         if(token == 'k')
         {
-            std::string key = encoded.substr(0,fieldlen);
-            encoded = encoded.substr(fieldlen);
-            token = encoded[0];
+            std::string key = encoded.substr(s,fieldlen);
+            s += fieldlen;
+            token = encoded[s];
             if(token == 's')
             {
-                std::stringstream subconv( encoded.substr(STORETYPELEN,LENGTHFIELD) );
+                std::stringstream subconv( encoded.substr(s+STORETYPELEN,LENGTHFIELD) );
                 size_t fieldlen;
                 subconv>>fieldlen;
-                encoded = encoded.substr(STORETYPELEN+LENGTHFIELD);
+                s += STORETYPELEN+LENGTHFIELD;
                 // Take the subtree and add it based on the key you just decoded
-                std::string subtree = encoded.substr(0,fieldlen);
+                std::string subtree = encoded.substr(s,fieldlen);
                 decoded.add_child( key, decode_tree( subtree ) );
-                encoded = encoded.substr( fieldlen );
+                s += fieldlen;
             }
         }
         else if(token == 'v')
         {
-            std::string v = encoded.substr(0,fieldlen);
+            std::string v = encoded.substr(s,fieldlen);
             // If the tree has no key, store the value in the root.
             decoded.put_value( v );
-            encoded = encoded.substr( fieldlen );
+            s += fieldlen;
         }
     }
     return decoded;

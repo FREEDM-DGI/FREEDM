1e2e766c3c12dd73c7c039faac1cdddb5b8b9d46
Michael Catanzaro <michael.catanzaro@mst.edu>
2013-07-01 11:14:56 -0500

    Add more rtds test cases!

--- Broker/testing/run_test.sh
+++ Broker/testing/run_test.sh
@@ -21,10 +21,10 @@ case $1 in
                 ../PosixBroker -c config/freedm.cfg
                 ;;
             "dgi1")
-                ../PosixBroker -c config/freedm.cfg --port 1870 --factory-port 53000 --add-host "$host:1871"
+                ../PosixBroker -c config/freedm.cfg --port 51870 --factory-port 53000 --add-host "$host:1871"
                 ;;
             "dgi2")
-                ../PosixBroker -c config/freedm.cfg --port 1871 --factory-port 56000 --add-host "$host:1870"
+                ../PosixBroker -c config/freedm.cfg --port 51871 --factory-port 56000 --add-host "$host:1870"
                 ;;
             "default")
                 ../PosixBroker -c config/freedm.cfg --factory-port 53000
@@ -45,11 +45,15 @@ case $1 in
                 sleep 3
                 killall PosixBroker
             ;;
-            "MultiDgi")
-                ../PosixBroker -v3 -c config/freedm.cfg --adapter-config config/adapters/$2A.xml &
-                ../PosixBroker -v3 -c config/freedm.cfg --adapter-config config/adapters/$2B.xml &
-                sleep 3
+            "MultipleDgi")
+                ../PosixBroker -v3 -c config/freedm.cfg --adapter-config config/adapters/"$2"A.xml --port 51870 --add-host "$host:51871" --add-host "$host:51872" --add-host "$host:51873" --add-host "$host:51874" &
+                ../PosixBroker -v3 -c config/freedm.cfg --adapter-config config/adapters/"$2"B.xml --port 51871 --add-host "$host:51870" --add-host "$host:51872" --add-host "$host:51873" --add-host "$host:51874" &
+                ../PosixBroker -v3 -c config/freedm.cfg --adapter-config config/adapters/"$2"C.xml --port 51872 --add-host "$host:51871" --add-host "$host:51870" --add-host "$host:51873" --add-host "$host:51874" &
+                ../PosixBroker -v3 -c config/freedm.cfg --adapter-config config/adapters/"$2"D.xml --port 51873 --add-host "$host:51871" --add-host "$host:51872" --add-host "$host:51870" --add-host "$host:51874" &
+                ../PosixBroker -v3 -c config/freedm.cfg --adapter-config config/adapters/"$2"E.xml --port 51874 --add-host "$host:51871" --add-host "$host:51872" --add-host "$host:51873" --add-host "$host:51870" &
+                sleep 15
                 killall PosixBroker
+                sleep 1
             ;;
             *)
                 echo "Invalid RTDS test type: $rtds_test_type"

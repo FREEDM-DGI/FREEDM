1edf884230cf3dd00a86a0b95fc84de1c50448fb
Stephen Jackson <scj7t4@gmail.com>
2014-06-06 09:41:59 -0500

    Impove loading, little more debug

--- Broker/src/CListener.cpp
+++ Broker/src/CListener.cpp
@@ -129,13 +129,9 @@ void CListener::HandleRead(const boost::system::error_code& e,
         ScheduleListen();
     }
 
-    std::stringstream iss;
-    std::ostreambuf_iterator<char> output(iss);
-    std::copy(m_buffer.begin(), m_buffer.begin()+bytes_transferred, output);
-
     Logger.Debug<<"Loading protobuf"<<std::endl;
     ProtocolMessage pm;
-    if(!pm.ParseFromIstream(&iss))
+    if(!pm.ParseFromArray(m_buffer.begin(), bytes_transferred))
     {
         Logger.Error<<"Failed to load protobuf"<<std::endl;
         ScheduleListen();
@@ -151,6 +147,7 @@ void CListener::HandleRead(const boost::system::error_code& e,
     }
 #endif
 
+    Logger.Debug<<"Fetching Connection"<<std::endl;
     std::string uuid = pm.source_uuid();
     /// We can make the remote host from the endpoint:
     SRemoteHost host = { m_recv_from.address().to_string(), boost::lexical_cast<std::string>(m_recv_from.port()) };

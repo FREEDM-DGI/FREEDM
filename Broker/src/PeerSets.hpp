c1a0cb59082658bcb8bc35065070b1dbec0e2b80
Stephen Jackson <scj7t4@gmail.com>
2014-08-28 13:31:05 -0500

    Another typename

--- Broker/src/PeerSets.hpp
+++ Broker/src/PeerSets.hpp
@@ -49,7 +49,7 @@ typedef std::map<std::string,
                  std::pair<CPeerNode, boost::posix_time::ptime> > TimedPeerSet;
 
 /// Provides a TimedPeerSet iterator templated on T
-typedef typename TimedPeerSet::iterator TimedPeerSetIterator;
+typedef TimedPeerSet::iterator TimedPeerSetIterator;
 
 /// Provides count() for a TimedPeerSet
 int CountInTimedPeerSet(TimedPeerSet& tps, const CPeerNode& m);

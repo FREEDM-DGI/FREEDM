74c5e01d39706cc65d14d814536cc7e9a17350f2
tprfh7 <tprfh7@mst.edu>
2015-04-29 14:37:38 -0500

    fixed sign error in failure message

--- Broker/src/pa/PhysicalAttestation.cpp
+++ Broker/src/pa/PhysicalAttestation.cpp
@@ -202,7 +202,7 @@ void PAAgent::CalculateInvariant(const Framework & framework)
 
     if(std::abs(actual_change - framework.expected_value) > ERROR_MARGIN)
     {
-        GetMe().Send(MessageAttestationFailure(framework.target, -framework.expected_value));
+        GetMe().Send(MessageAttestationFailure(framework.target, framework.expected_value));
     }
 }
 

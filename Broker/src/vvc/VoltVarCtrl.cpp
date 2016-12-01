a29fafc25f072def6d9b8f372a6846732a7646a5
Yue Shi <yueshi1989@gmail.com>
2016-12-01 16:33:51 -0500

    Update VoltVarCtrl.cpp

--- Broker/src/vvc/VoltVarCtrl.cpp
+++ Broker/src/vvc/VoltVarCtrl.cpp
@@ -1488,8 +1488,9 @@ if (Ploss_nsize>Ploss_osize)
   step_size;
  
   // send messages to slaves
-	if (Ploss_osize < Ploss_orig)
-  	BOOST_FOREACH(CPeerNode peer, m_peers | boost::adaptors::map_values)
+	if (Ploss_osize < Ploss_orig)// grad message will NOT be sent to slaves if loss is not reduced
+	{
+		BOOST_FOREACH(CPeerNode peer, m_peers | boost::adaptors::map_values)
         	{
       	    
 	    	ModuleMessage mm = VoltageDelta(2, 3.0, "NCSU");
@@ -1503,6 +1504,7 @@ if (Ploss_nsize>Ploss_osize)
 	    	peer.Send(mg);
 	    
 		}
+	}
 	else
 	{
 	}
@@ -1707,19 +1709,26 @@ if (Ploss_nsize>Ploss_osize)
   step_size;
   
   // send messages to slaves
-  BOOST_FOREACH(CPeerNode peer, m_peers | boost::adaptors::map_values)
-        {
+	if (Ploss_osize < Ploss_orig)
+	{
+  	BOOST_FOREACH(CPeerNode peer, m_peers | boost::adaptors::map_values)
+        	{
       	    
-	    ModuleMessage mm = VoltageDelta(2, 3.0, "Gradients reversed!");
-            peer.Send(mm);
+	    	ModuleMessage mm = VoltageDelta(2, 3.0, "Gradients reversed!");
+            	peer.Send(mm);
 	    
 	   
-	    mat S2;
-	    S2 << Dl(1,7) << Dl(2,7) << Dl(3,7) << Dl(4,7) << Dl(6,7) << Dl(7,7) << Dl(8,7) << Dl(1,9) << Dl(2,9) << Dl(3,9) << Dl(4,9) << Dl(6,9) << Dl(7,9) << Dl(8,9) << Dl(1,11)<< Dl(2,11)<< Dl(3,11)<< Dl(4,11)<< Dl(6,11)<< Dl(7,11)<< Dl(8,11)<< endr;
-	    S2 = S2.t();
-	    ModuleMessage mg = Gradient(S2);
-	    peer.Send(mg);
+	    	mat S2;
+	    	S2 << Dl(1,7) << Dl(2,7) << Dl(3,7) << Dl(4,7) << Dl(6,7) << Dl(7,7) << Dl(8,7) << Dl(1,9) << Dl(2,9) << Dl(3,9) << Dl(4,9) << Dl(6,9) << Dl(7,9) << Dl(8,9) << Dl(1,11)<< Dl(2,11)<< Dl(3,11)<< Dl(4,11)<< Dl(6,11)<< Dl(7,11)<< Dl(8,11)<< endr;
+	    	S2 = S2.t();
+	    	ModuleMessage mg = Gradient(S2);
+	    	peer.Send(mg);
+		}
 	}
+	else
+	{
+	}
+	
 	
   break;
 }
